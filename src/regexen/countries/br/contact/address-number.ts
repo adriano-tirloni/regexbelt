e2aba81e8ae5c763efde0046c738360981fc4489
Adriano Tirloni <tirloni.adriano@gmail.com>
2026-04-09 05:57:19 -0300

    remove br address number

--- src/regexen/countries/br/contact/address-number.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Matches a Brazilian address number: one or more digits, or S/N (case-insensitive)
- *
- * ___Enforces beginning and end of string___
- * @example ✅ '123'
- * @example ❌ 'ABC'
- */
-export const addressNumber = /^(?:\d+|[Ss]\/[Nn])$/;
